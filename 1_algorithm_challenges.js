86da36fa0af9d4523eca58c3ee6aa826679c9683
Rakibul-Hasan <rakib.it.ergon@gmail.com>
2025-05-19 11:59:17 +0600

    JavaScript solved 01

---
+++ 1_algorithm_challenges.js
@@ -0,0 +1,22 @@
+// Give an array integers, replace all the occurrences of element to replace with subtraction elem.
+
+// Example
+// 	For inputArray = [1, 2, 1], element to Replace = 1 and subtractionElem = 3, the output should be arrayReplace (inputArray, elementToReplace, subtractionElem) = [3, 2, 3}.
+// Input / output
+// [input] array.integer inputArray
+
+// Code Solution: 
+
+// let arrayReplace = function(inputArray, elementToReplace, subtractionElem){
+    
+//     inputArray.forEach((element, index) => {
+//        if(element === elementToReplace){
+//         inputArray[index] = subtractionElem
+//        }
+//     });
+//     console.log(inputArray)
+// }
+
+// arrayReplace([1,2,1],1,3);
+
+
